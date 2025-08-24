7a9742dbdd3ab0aa17fd6d62793bf1312eb898eb
Arumuga Selvi S <arumugaselvi04_bai27@mepcoeng.ac.in>
2025-08-24 16:53:41 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,42 @@
+function abc()
+{
+    console.log("Heello")
+}
+abc()
+function oppo()
+{
+    console.log("This is oppo")
+}
+oppo()
+var a=10;
+var b=20;
+function add()
+{
+    console.log(a+b)
+}
+add()
+var factor="sk";
+var fplayer="dhoni";
+var fmovie="ddd";
+function special()
+{
+    console.log("Favourite Actor:"+factor)
+    console.log("Favourite player:"+fplayer)
+}
+special()
+
+function area(length,breadth)
+{
+    console.log("Area:"+length*breadth)
+}
+area(10,20)
+
+
+
+function add(a,b)
+{
+    return a+b;
+    
+}
+var total=add(10,20)
+console.log(total)
