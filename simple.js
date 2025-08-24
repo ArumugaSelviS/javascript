8ae588912ac99e0d1d85206bec54ed77418df7b2
Arumuga Selvi S <arumugaselvi04_bai27@mepcoeng.ac.in>
2025-08-24 16:36:49 +0530

    Add files via upload

---
+++ simple.js
@@ -0,0 +1,16 @@
+var price=100;
+var product="Soap";
+var tax=10;
+console.log(product)
+total=price+tax;
+console.log(total)
+
+var fruitname="apple";
+var count=12;
+var price=100;
+var total=count*price;
+console.log(fruitname)
+console.log(total)
+
+console.log(typeof("apple"))
+console.log(typeof(10))
